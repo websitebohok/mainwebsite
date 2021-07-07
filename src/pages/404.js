781590768aed5c639ad8f7f6826f530f8c637c5e
astabidasaaa <astabidasabcd@gmail.com>
2021-07-07 23:41:03 +0700

    error page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Banner from "../components/Banner"
+
+const NotFoundPage = () => (
+  <>
+    <Banner content="Error 404" />
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </>
+)
+
+export default NotFoundPage
