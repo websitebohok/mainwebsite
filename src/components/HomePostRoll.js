09e302f671d5367d89cacb95c88e0c42fbdf1fbb
astabidasaaa <astabidasabcd@gmail.com>
2021-09-02 06:12:51 +0700

    removing pranala ternary

--- src/components/HomePostRoll.js
+++ src/components/HomePostRoll.js
@@ -3,25 +3,13 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 import PropTypes from "prop-types"
 
-const _ = require("lodash")
-
 const PostsItemStyle = styled.article`
   margin-top: 1.5rem;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: stretch;
-  // max-width: 25rem;
-  // min-width: 20rem;
 
-  // @media (min-width: 768px) {
-  //   margin: calc(var(--spacing) * 4) 0;
-  // }
-
-  // @media (min-width: 1200px) {
-  //   margin: calc(var(--spacing) * 4) 0;
-
-  // }
   .hp-category {
     text-transform: capitalize;
     color: var(--black);
@@ -52,35 +40,22 @@ const PostsItemStyle = styled.article`
 const HomePostRoll = ({ nodeObj }) => {
   const {
     fields: { slug },
-    frontmatter: { title, description, date, path, category, webname },
+    frontmatter: { title, description, date, category },
     timeToRead,
   } = nodeObj
 
   return (
     <PostsItemStyle>
-      {category === "links" ? (
-        <a href={path} target="_blank" rel="noopener noreferrer">
-          {category && <p className="hp-category">{category}</p>}
-          {title && <h5>{title}</h5>}
-          {description && <p>{description}</p>}
-          <div className="meta">
-            {date && <p className="subPara">{date}</p>}
-            <p className="subPara bullet">•</p>
-            {timeToRead && <p className="subPara">{_.capitalize(webname)}</p>}
-          </div>
-        </a>
-      ) : (
-        <Link to={slug}>
-          {category && <p className="hp-category">{category}</p>}
-          {title && <h5>{title}</h5>}
-          {description && <p>{description}</p>}
-          <div className="meta">
-            {date && <p className="subPara">{date}</p>}
-            <p className="subPara bullet">•</p>
-            {timeToRead && <p className="subPara">{timeToRead} min read</p>}
-          </div>
-        </Link>
-      )}
+      <Link to={slug}>
+        {category && <p className="hp-category">{category}</p>}
+        {title && <h5>{title}</h5>}
+        {description && <p>{description}</p>}
+        <div className="meta">
+          {date && <p className="subPara">{date}</p>}
+          <p className="subPara bullet">•</p>
+          {timeToRead && <p className="subPara">{timeToRead} min read</p>}
+        </div>
+      </Link>
     </PostsItemStyle>
   )
 }
@@ -88,11 +63,9 @@ const HomePostRoll = ({ nodeObj }) => {
 HomePostRoll.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  path: PropTypes.string,
   slug: PropTypes.string,
   date: PropTypes.string,
   category: PropTypes.string,
-  webname: PropTypes.string,
 }
 
 export default HomePostRoll
