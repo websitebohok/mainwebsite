d64d0d006bd7120c072da7e56b64785207e89582
websitebohok <websitebohok@gmail.com>
2021-09-21 18:04:43 +0700

    adding figure in cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,9 @@
 import CMS from "netlify-cms-app"
 import cloudinary from "netlify-cms-media-library-cloudinary"
 import caption from "./caption"
+import { Figure } from "netlify-cms-editor-component-figure"
 
 CMS.registerMediaLibrary(cloudinary)
 //Register Editor Components
 CMS.registerEditorComponent(caption)
+CMS.registerEditorComponent(Figure)
