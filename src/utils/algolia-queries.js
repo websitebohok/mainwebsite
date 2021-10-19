d1aa21c8a9e68c60acebffeeb871400a2c1f02d6
websitebohok <websitebohok@gmail.com>
2021-10-20 00:23:39 +0700

    unmount search query

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -1,47 +1,47 @@
-const escapeStringRegexp = require("escape-string-regexp")
+// const escapeStringRegexp = require("escape-string-regexp")
 
-const pagePath = `src/content`
-const indexName = `Bohok`
+// const pagePath = `src/content`
+// const indexName = `Bohok`
 
-const pageQuery = `{
-  pages: allMarkdownRemark(
-    filter: {
-      fileAbsolutePath: { regex: "/${escapeStringRegexp(pagePath)}/" },
-      frontmatter: {category: {ne: "pranala"}}
-    }
-  ) {
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-          category
-        }
-        fields {
-          slug
-        }
-        excerpt(pruneLength: 5000)
-      }
-    }
-  }
-}`
+// const pageQuery = `{
+//   pages: allMarkdownRemark(
+//     filter: {
+//       fileAbsolutePath: { regex: "/${escapeStringRegexp(pagePath)}/" },
+//       frontmatter: {category: {ne: "pranala"}}
+//     }
+//   ) {
+//     edges {
+//       node {
+//         id
+//         frontmatter {
+//           title
+//           category
+//         }
+//         fields {
+//           slug
+//         }
+//         excerpt(pruneLength: 5000)
+//       }
+//     }
+//   }
+// }`
 
-function pageToAlgoliaRecord({ node: { id, frontmatter, fields, ...rest } }) {
-  return {
-    objectID: id,
-    ...frontmatter,
-    ...fields,
-    ...rest,
-  }
-}
+// function pageToAlgoliaRecord({ node: { id, frontmatter, fields, ...rest } }) {
+//   return {
+//     objectID: id,
+//     ...frontmatter,
+//     ...fields,
+//     ...rest,
+//   }
+// }
 
-const queries = [
-  {
-    query: pageQuery,
-    transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
-    indexName,
-    settings: { attributesToSnippet: [`excerpt:20`] },
-  },
-]
+// const queries = [
+//   {
+//     query: pageQuery,
+//     transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
+//     indexName,
+//     settings: { attributesToSnippet: [`excerpt:20`] },
+//   },
+// ]
 
-module.exports = queries
+// module.exports = queries
